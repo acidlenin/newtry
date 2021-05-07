834dcfb963c4bf13f74b2000bfb3b90a34f9014d
Volodya <acid_lenin@ukr.net>
2021-05-07 11:49:23 +0300

    Updates

---
+++ precache-manifest.ec94b36513728f1acbe64fa58221c94a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0e448bd4e1b9dee63d0fa44dde532f",
+    "url": "/newtry/index.html"
+  },
+  {
+    "revision": "a12d3308a17cc3e8d44e",
+    "url": "/newtry/static/css/3.3a0091ec.chunk.css"
+  },
+  {
+    "revision": "c719167875f29a36e121",
+    "url": "/newtry/static/css/4.10e04a2b.chunk.css"
+  },
+  {
+    "revision": "9965e7920cd2b4d28779",
+    "url": "/newtry/static/css/main.b588a5e4.chunk.css"
+  },
+  {
+    "revision": "89365c641ddcf9e629bd",
+    "url": "/newtry/static/js/2.45003efe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/newtry/static/js/2.45003efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12d3308a17cc3e8d44e",
+    "url": "/newtry/static/js/3.6352cca6.chunk.js"
+  },
+  {
+    "revision": "c719167875f29a36e121",
+    "url": "/newtry/static/js/4.597944ea.chunk.js"
+  },
+  {
+    "revision": "9965e7920cd2b4d28779",
+    "url": "/newtry/static/js/main.8c50493c.chunk.js"
+  },
+  {
+    "revision": "851df7a3c3cb4033ebf6",
+    "url": "/newtry/static/js/runtime-main.0efea098.js"
+  },
+  {
+    "revision": "d47e58c71a6e6573f1e35fcb2beeee2a",
+    "url": "/newtry/static/media/Youtube_loading_symbol_1_(wobbly).d47e58c7.gif"
+  }
+]);
